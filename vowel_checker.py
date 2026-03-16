eada7659c99e8eb3117bb06c691a42f55b189418
RezaPaseban1991 <rezapaseban70@yahoo.com>
2026-03-16 09:49:14 -0700

    Create vowel_checker.py
    Add vowel checker program using conditional statements

---
+++ vowel_checker.py
@@ -0,0 +1,28 @@
+#the determine whether the character entered is a vowel or not
+
+#input
+char = input('Enter any character : ')
+
+#calculation/output
+if char == 'a':
+    print(char, 'is a vowel')
+elif char == 'e':
+    print(char, 'is a vowel')
+elif char == 'i':
+    print(char, 'is a vowel')
+elif char == 'o':
+    print(char, 'is a vowel')
+elif char == 'u':
+    print(char, 'is a vowel')
+elif char == 'A':
+    print(char, 'is a vowel')
+elif char == 'E':
+    print(char, 'is a vowel')
+elif char == 'I':
+    print(char, 'is a vowel')
+elif char == 'O':
+    print(char, 'is a vowel')
+elif char == 'U':
+    print(char, 'is a vowel')
+else:
+    print(char, 'is not a vowel')
